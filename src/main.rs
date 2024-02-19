dc523f8accca97d7658e61060215c85837b4ccb7
brox <nvanamen@netreo.com>
2024-02-19 12:28:48 -0800

    -init

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
-use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-   HttpResponse::Ok()
-}
-
+use std::io::Error;
+use actix_web::dev::Server;
+use zero2prod::run;
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("0.0.0.0:8000")?
-        .run()
-        .await
+async fn main() -> actix_web::Result<(), Error> {
+    run()?.await
     }
+
+

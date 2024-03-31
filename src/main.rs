401e5bb358cdff68dc619cd9487b891459f0cc71
brox <nvanamen@netreo.com>
2024-03-31 15:31:11 -0700

    adding sqlx config and github actions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::io;
-use env_logger::Env;
 
 use sqlx::PgPool;
+use tracing_log::LogTracer;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
     // Fetch configuration once
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to Set logger");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     // Connect to the PostgreSQL database
